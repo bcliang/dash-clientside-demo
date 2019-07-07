6b363e497ecf38ffc76d48efb628cac9d7927066
bcliang <brad@bliang.com>
2019-07-06 20:27:24 -0700

    add gunicorn as a dependency

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ VERSION = None  # get this from __version__.py
 
 
 # What packages are required for this module to be executed?
-REQUIRED = ["dash>=0.40.0", "dash-bootstrap-components>=0.3.0", "click", "dash-extendable-graph"]
+REQUIRED = ["dash>=1.0.0", "dash-bootstrap-components>=0.3.0", "click", "dash-extendable-graph", "gunicorn"]
 
 # What packages are optional?
 EXTRAS = {
